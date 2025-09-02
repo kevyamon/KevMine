e5128c0d297a05a43e2470a7fb50927a39b9eeda
baticlean <baticlean225@gmail.com>
2025-09-02 11:38:27 +0000

    Rect

--- src/components/PrivateRoutes.jsx
+++ src/components/PrivateRoutes.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { Navigate, Outlet } from 'react-router-dom';
+import { Navigate, Outlet, useOutletContext } from 'react-router-dom';
 
 const PrivateRoutes = () => {
   const { userInfo } = useSelector((state) => state.auth);
+  const context = useOutletContext(); // On récupère le contexte venant de App.jsx
 
   if (!userInfo) {
     return <Navigate to="/login" replace />;
   }
 
-  // NOUVEAU : Vérification du statut
   if (userInfo.status === 'banned' || userInfo.status === 'suspended') {
     return <Navigate to="/banned" replace />;
   }
 
-  return <Outlet />;
+  // On passe le contexte récupéré aux routes enfants (comme NotificationsScreen)
+  return <Outlet context={context} />;
 };
 
 export default PrivateRoutes;
